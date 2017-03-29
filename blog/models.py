1f7005f38a946cf8db2aa2d538d2b88747a337dc
sergei <s.chichin@gmail.com>
2017-03-29 11:27:41 +0700

    добавил строку со гггггг

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,11 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+#    author = models.ForeignKey('auth.User')
+#    posts1 = open('buf.txt').read()
     title = models.CharField(max_length=200)
     text = models.TextField()
+    text = 'ggggggg'
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
